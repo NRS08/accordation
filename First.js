94f331fe80676efb02fdd0233326e3c99aa77c85
Omkar <omkarkhatale1002@gmail.com>
2022-10-27 00:16:01 +0530

    Added First Page

---
+++ First.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const First = () => {
+  return (
+    <div>
+      <h1> First Page</h1>
+
+      <div class="content">
+            <h1>Hello World</h1>
+      </div>
+    </div>
+  )
+}
+
+export default First
